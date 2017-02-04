95a331e539549ff9be49eb7a8dfdf40d2c1730fa
Shashank Singh <cse.shashanksingh@gmail.com>
2017-02-04 19:04:07 +0530

    User login started

---
+++ login.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fragger
+ * Date: 2/4/17
+ * Time: 6:52 PM
+ */
+
+?>
+
+<html>
+<head>
+    <title>CoderCup-Login</title>
+
+    <style>
+        body{
+            background-color: rgb(63,76,107);
+        }
+        .heading-container{
+            margin: auto;
+            margin-top:100px;
+            width:400px;
+            background-color: whitesmoke;
+            padding:2px 10px;
+            border-radius: 8px 8px 0 0;
+            box-shadow: grey 10px 10px 5px;
+        }
+        .main-container{
+            margin:auto;
+            margin-top:20px;
+            width:400px;
+            padding:10px;
+            background-color: white;
+            min-height:300px;
+            border-radius: 0 0 8px 8px;
+            box-shadow: grey 10px 10px 5px;
+
+        }
+
+    </style>
+</head>
+
+<body>
+    <div class="heading-container">
+        <h2 align="center" style="color:darkred;font-family: mono, sans-serif">&lt/CoderCup&gt Login</h2>
+    </div>
+    <div class="main-container">
+
+    </div>
+</body>
+
+</html>
