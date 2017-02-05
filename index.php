f18a4f67be77fbbb0643e2fcf7c4a32ca8398b32
hexnor <cse.ykr@gmail.com>
2017-02-05 18:21:26 +0530

    Done with Timer !!!!!!!!

--- index.php
+++ index.php
@@ -69,29 +69,10 @@ if (isset($_GET['success']))
     ?>
     </ul>
 </div>
+<script type="text/javascript" src="js/timer.js">
 
+</script>
 <script type="text/javascript" >
-var timeoutHandle;
-//DEFINING OF SESSION REMAINING !!!!
-function timelimit(minutes) {
-    var seconds = 60;
-    var mins = minutes;
-    function update() {
-        var counter = document.getElementById("timer_text");
-        var current_minutes = mins-1
-        seconds--;
-        counter.innerHTML =current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
-        if( seconds > 0 ) {
-            timeoutHandle=setTimeout(update, 1000);
-        } else {
-            if(mins > 1){
-               setTimeout(function () { timelimit(mins - 1); }, 1000);
-            }
-        }
-    }
-    //UPDATE METHOD TO UPDATE AT REGULAR INTERVALS
-    update();
-}
 timelimit(Math.floor(<?php echo getRemTimeForUser($con, $_SESSION['username']); ?>/60));
 </script>
 
