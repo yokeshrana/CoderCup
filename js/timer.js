3b36034211c8f053efdd55f353e9be60e625e031
Shashank Singh <cse.shashanksingh@gmail.com>
2017-02-15 16:59:25 +0530

    removed comment

--- js/timer.js
+++ js/timer.js
@@ -13,16 +13,6 @@ function timelimit(time) {
     var mins = Math.floor(time/60);
     function update() {
         var counter = document.getElementById("timer_text");
-        // var current_minutes = mins;
-        // seconds--;
-        // counter.innerHTML =current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
-        // if( seconds > 0 ) {
-        //     timeoutHandle=setTimeout(update, 1000);
-        // } else {
-        //     if(mins > 1){
-        //        setTimeout(function () { timelimit(mins - 1); }, 1000);
-        //     }
-        // }
         counter.innerHTML =mins.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
         seconds--;
         if(seconds>0)
